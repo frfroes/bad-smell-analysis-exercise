b98a4a5811f52fa97d88cea33d2321bab3a470f3
Filipe Fróes <fr.froes@gmail.com>
2018-05-23 17:13:58 -0300

    Implementation of item 1.d

--- analysis.R
+++ analysis.R
@@ -1,5 +1,5 @@
 # 1º Item
-# Read from aprils csv to data frame
+# Read from aprils csv and stores it on a data frame
 aprilsbs.df <- read.csv(file="data/bad_smells_abril.csv", header=TRUE)
 
 # 1.a sum of all aprils bad smells 
@@ -10,5 +10,13 @@ paste("O total de bad smells em abril é de", aprilsbs.sum)
 aprilsbs.avg <- aprilsbs.sum / ncol(aprilsbs.df[,-1:-2])
 paste("A média total me abril de bad smells em todos os softwares é de", aprilsbs.avg)
 
+# 1.c software with highest bad smells count of april
 aprilsbs.toppkg <- aprilsbs.df[which.max(rowSums(aprilsbs.df[,-1:-2])), "package_name"]
 paste("O software com mais bad smells am abril é o", aprilsbs.toppkg)
+
+# 1.d plot the total amout of bad smells by type
+bs.colsums <- colSums(aprilsbs.df[,-1:-2])
+ylim <- c(0, 1.3*max(bs.colsums)) # used to scale the yaxis so the highest bar fits it
+bs.colsums.plot <- barplot(bs.colsums, main="Total de bad smells por tipo - Abril", ylim = ylim,
+        ylab="Quantidade de bad smells", xlab="Tipo de bad smell")
+text(x=bs.colsums.plot, y=bs.colsums, label=bs.colsums, pos=3, cex=0.8) # adds text at top of bars
