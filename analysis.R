fa2d30f2edcef42d9e2cb5073c6973cc9f3332b9
Filipe Fróes <fr.froes@gmail.com>
2018-05-23 15:24:54 -0300

    Implementation of item 1.a

--- analysis.R
+++ analysis.R
@@ -1,3 +1,8 @@
-library(readr)
-bad_smells_abril <- read_csv("data/bad_smells_abril.csv")
 
+# 1º Item
+# Read from aprils csv to data frame
+aprilsbs.df <- read.csv(file="data/bad_smells_abril.csv", header=TRUE)
+
+# 1.a sum of all aprils bad smells 
+aprilsbs.sum <- sum(bsabril.df[,-1:-2])
+aprilsbs.sum
