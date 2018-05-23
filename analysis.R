dc17b291c9acd769bad5093eed2ed8e6c0fd3cae
Filipe Fróes <fr.froes@gmail.com>
2018-05-23 17:31:49 -0300

    Impementation of item 1.e

--- analysis.R
+++ analysis.R
@@ -20,3 +20,11 @@ ylim <- c(0, 1.3*max(bs.colsums)) # used to scale the yaxis so the highest bar f
 bs.colsums.plot <- barplot(bs.colsums, main="Total de bad smells por tipo - Abril", ylim = ylim,
         ylab="Quantidade de bad smells", xlab="Tipo de bad smell")
 text(x=bs.colsums.plot, y=bs.colsums, label=bs.colsums, pos=3, cex=0.8) # adds text at top of bars
+
+# 1.e Variance and standart deviation of bad smells types
+bs.colvars <- apply(aprilsbs.df[,-1:-2], 2, var)
+bs.colsds <- apply(aprilsbs.df[,-1:-2], 2, sd)
+paste("A variância dos bad smells do mês por tipo são:")
+bs.colvars
+paste("A variância dos bad smells do mês por tipo são:")
+bs.colsds
