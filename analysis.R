62a2f404932f531b326dd09c42bc54d1c13391a2
Filipe Fróes <fr.froes@gmail.com>
2018-05-23 15:49:26 -0300

    Implementation of item 1.b

--- analysis.R
+++ analysis.R
@@ -1,8 +1,12 @@
-
 # 1º Item
 # Read from aprils csv to data frame
 aprilsbs.df <- read.csv(file="data/bad_smells_abril.csv", header=TRUE)
 
 # 1.a sum of all aprils bad smells 
-aprilsbs.sum <- sum(bsabril.df[,-1:-2])
-aprilsbs.sum
+aprilsbs.sum <- sum(aprilsbs.df[,-1:-2])
+cat("O total de bad smells em abril é de", aprilsbs.sum)
+
+# 1.b total average of all aprils bad smells 
+aprilsbs.avg <- aprilsbs.sum / ncol(aprilsbs.df[,-1:-2])
+cat("A média total me abril de bad smells em todos os softwares é de", aprilsbs.avg)
+
